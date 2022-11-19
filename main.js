1258922236fa76dc4b9c35c255895abbcfb2ed63
Avi <81462673+Cosmos365@users.noreply.github.com>
2022-11-19 18:21:13 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+nosex = 0;
+nosey = 0;
 
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/RF2f4Fzr/bigote-o-moustache-png-by-ohyeahisniaeditions-on-deviantart-5.png');
 }
 
 function setup() {
@@ -15,10 +18,12 @@ function setup() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = "+results[0].pose.nose.x);
-        console.log("nose y = "+results[0].pose.nose.y);
-    };
-};
+        nosex = results[0].pose.nose.x;
+        nosey = results[0].pose.nose.y-15;
+        console.log("nose x = "+nosex);
+        console.log("nose y = "+nosey);
+    };  
+}
 
 function modelLoaded() {
     console.log('model loaded');
@@ -26,8 +31,12 @@ function modelLoaded() {
 
 function draw() {
     image(video,0,0,300,300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    //circle(nosex,nosey,20);
+    image(mustache,nosex,nosey,30,30);
 }
 
-function takepic() {
+function take_snapshot() {
     save('Snsh.png');
 }
